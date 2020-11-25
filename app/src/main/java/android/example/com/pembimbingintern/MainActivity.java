c8dd6a088ce53ce05b3be50ea45a5c79acd87c06
SHARAHRIZKYNADYASTUTY <48179385+SHARAHRIZKYNADYASTUTY@users.noreply.github.com>
2020-11-25 15:24:44 +0700

    ty

--- app/src/main/java/android/example/com/pembimbingintern/MainActivity.java
+++ app/src/main/java/android/example/com/pembimbingintern/MainActivity.java
@@ -1,14 +1,42 @@
 package android.example.com.pembimbingintern;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatRatingBar;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatRatingBar ratingBar;
+    private Button btRating;
+    private TextView tvRating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ratingBar = (AppCompatRatingBar) findViewById(R.id.penilaian);
+        btRating = (Button) findViewById(R.id.submit);
+        tvRating = (TextView) findViewById(R.id.rate);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                tvRating.setText("Rate : "+rating);
+            }
+        });
+
+        btRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this," Rating "+ratingBar.getRating(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
